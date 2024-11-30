36fb7bc2f144892e5191e3e1b577d599ed035e33
Pablo <pablito@me.com>
2024-11-30 20:41:47 +0000

    chore: updated happy tests.

--- app.test.js
+++ app.test.js
@@ -108,45 +108,80 @@ describe('App todo endpoint integration tests', () => {
     })
   });
 
-  // describe('UPDATE /todos/:id', () => {
-  //   test.each([
-  //     ['123456789012345678901234', 'Eat', false],
-  //     ['234567890123456789012345', 'Dream', true],
-  //     ['345678901234567890123456', 'Pray', true],
-  //     ['123456789012345678901234', 'Swim', false],
-  //     ['234567890123456789012345', 'Dream', false],
-  //   ])('should update todo in the database and return status 201 and an array with the updated todo object with id, "%s"', async (id, task, completed) => {
-  //     // Act
-  //     const response = await request(app).put(`/todos/${id}`).send({ task, completed });
-  //
-  //     // Assert
-  //     expect(response.status).toBe(201);
-  //     expect(response.body).toEqual([{ id, task, completed }]);
-  //     expect(response.body[0].id).toBe(id);
-  //     expect(response.body[0].task).toBe(task);
-  //     expect(response.body[0].completed).toBe(completed);
-  //   });
-  //
-  //   test.each([
-  //     [1, '', false, 400, 'Task and completed properties are required to update this todo.'],
-  //     [2, 'Dream', undefined, 400, 'Task and completed properties are required to update this todo.'],
-  //     [3, 'Dream', 'fish', 400, 'The completed property must be of type boolean.'],
-  //     [1, 'Dream', 212, 400, 'The completed property must be of type boolean.'],
-  //     [2, true, true, 400, 'The task property must be of type string.'],
-  //     [3, 212, true, 400, 'The task property must be of type string.'],
-  //     ['999999999999999999999999', 'Fly', 'pig', 400, 'The completed property must be of type boolean.'],
-  //     ['cat', 'Fly', true, 400, 'Invalid id provided. ID must be a number.'],
-  //     ['999999999999999999999999', 'Fly', true, 404, 'No todo with an ID of 999999999999999999999999 could be found in the database.'],
-  //   ])('should return an appropriate status and error message when passed the ID param, "%s"', async (id, task, completed, status, errorMessage) => {
-  //     // Act
-  //     const response = await request(app).put(`/todos/${id}`).send({ task, completed });
-  //
-  //     // Assert
-  //     expect(response.status).toBe(status);
-  //     expect(response.body.message).toBe(errorMessage);
-  //     expect(response.body).toEqual({ message: errorMessage });
-  //   });
-  // });
+  describe('UPDATE /todos/:id', () => {
+    test.each([
+      ['123456789012345678901234', 'Eat', false],
+      ['234567890123456789012345', 'Dream', true],
+      ['345678901234567890123456', 'Pray', true],
+      ['123456789012345678901234', 'Swim', false],
+      ['234567890123456789012345', 'Dream', false],
+    ])('should update todo in the database and return status 200 and an array with the updated todo object with id, "%s"', async (id, task, completed) => {
+      // Act
+      const response = await request(app).patch(`/todos/${id}`).send({ task, completed })
+
+      // Assert
+      expect(response.status).toBe(200)
+      const updatedTodo = response.body
+      expect(updatedTodo).toMatchObject({ _id: id, task, completed });
+      expect((updatedTodo._id).toString()).toBe(id);
+      expect(updatedTodo.task).toBe(task);
+      expect(updatedTodo.completed).toBe(completed);
+    });
+
+    test.each([
+      ['123456789012345678901234', 'Go on holiday', true],
+      ['234567890123456789012345', 'Dream', false],
+      ['345678901234567890123456', 'Surf', false],
+    ])('should update todo in the database and return status 200 and an array with the updated todo object with id, "%s" when only the task property, %s, is passed in', async (id, task, completed) => {
+      // Act
+      const response = await request(app).patch(`/todos/${id}`).send({ task })
+
+      // Assert
+      expect(response.status).toBe(200)
+      const updatedTodo = response.body
+      expect(updatedTodo).toMatchObject({ _id: id, task, completed });
+      expect((updatedTodo._id).toString()).toBe(id);
+      expect(updatedTodo.task).toBe(task);
+      expect(updatedTodo.completed).toBe(completed);
+    });
+
+    test.each([
+      ['123456789012345678901234', 'Eat', false],
+      ['234567890123456789012345', 'Sleep', true],
+      ['345678901234567890123456', 'Pray', true],
+    ])('should update todo in the database and return status 200 and an array with the updated todo object with id, "%s" when only the completed property, %s, is passed in', async (id, task, completed) => {
+      // Act
+      const response = await request(app).patch(`/todos/${id}`).send({ completed })
+
+      // Assert
+      expect(response.status).toBe(200)
+      const updatedTodo = response.body
+      expect(updatedTodo).toMatchObject({ _id: id, task, completed });
+      expect((updatedTodo._id).toString()).toBe(id);
+      expect(updatedTodo.task).toBe(task);
+      expect(updatedTodo.completed).toBe(completed);
+    });
+
+    // test.each([
+    //   [1, '', false, 400, 'Task and completed properties are required to update this todo.'],
+    //   [2, 'Dream', undefined, 400, 'Task and completed properties are required to update this todo.'],
+    //   [3, 'Dream', 'fish', 400, 'The completed property must be of type boolean.'],
+    //   [1, 'Dream', 212, 400, 'The completed property must be of type boolean.'],
+    //   [2, true, true, 400, 'The task property must be of type string.'],
+    //   [3, 212, true, 400, 'The task property must be of type string.'],
+    //   ['999999999999999999999999', 'Fly', 'pig', 400, 'The completed property must be of type boolean.'],
+    //   ['cat', 'Fly', true, 400, 'Invalid id provided. ID must be a number.'],
+    //   ['999999999999999999999999', 'Fly', true, 404, 'No todo with an ID of 999999999999999999999999 could be found in the database.'],
+    // ])('should return an appropriate status and error message when passed the ID param, "%s"', async (id, task, completed, status, errorMessage) => {
+    //   // Act
+    //   const response = await request(app).patch(`/todos/${id}`).send({ task, completed });
+    //
+    //   // Assert
+    //   expect(response.status).toBe(status);
+    //   expect(response.body.message).toBe(errorMessage);
+    //   expect(response.body).toEqual({ message: errorMessage });
+    // });
+  });
 });
 
 
